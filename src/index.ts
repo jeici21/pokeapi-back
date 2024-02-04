166216811de0895ddbb939ecec7c314f1806b8c2
Jorge Castro <david92_jc@yahoo.es>
2024-02-04 16:06:12 -0500

    dependencias actualizadas

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,11 @@ import swaggerConfig from './swagger-config';
 const app = express()
 const PORT = Number(process.env.PORT) || 5000
 export const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT)
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT)
 });
 
 app.use(cors({ origin: "*" }));
@@ -24,8 +24,8 @@ swaggerConfig(app);
 app.get("/", getAllPokemonController)
 
 pool.connect().then(async () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
-    app.listen(PORT, '0.0.0.0');
-    await createTypeTable()
-    await createPokemonTable();
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0');
+  await createTypeTable()
+  await createPokemonTable();
 });
